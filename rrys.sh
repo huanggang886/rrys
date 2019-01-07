77a6d4c66de0c7bb3e88ad92f057dfccdfc9be3f
huanggang886 <frank.v.buono@maine.edu>
2019-01-07 16:58:55 +0800

    Create rrys.sh

---
+++ rrys.sh
@@ -0,0 +1,23 @@
+sudo mkfs.xfs /dev/xvdf
+
+sudo mkdir /home/ss
+
+sudo mount /dev/xvdf  /home/ss
+
+yum install wget -y
+
+wget --no-check-certificate https://sign.obagg.com/rrshareweb.zip && chmod +x rrshareweb.zip
+
+yum install zip unzip -y
+
+unzip rrshareweb.zip
+
+chmod -R 777 /root/rrshareweb
+
+chmod -R 777 /home/ss
+
+cd rrshareweb
+
+yum install screen -y
+
+screen ./rrshareweb 
